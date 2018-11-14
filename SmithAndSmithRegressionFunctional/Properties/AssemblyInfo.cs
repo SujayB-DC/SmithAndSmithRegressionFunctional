32205c8bef08243b0132389ec8a61ba92ffb45d8
Sujay Barooah <SujayB@datacom.co.nz>
2018-11-15 11:37:19 +1300

    initial commit

---
+++ SmithAndSmithRegressionFunctional/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SmithAndSmithRegressionFunctional")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmithAndSmithRegressionFunctional")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df971306-99b0-446c-8c5f-359c4fe588aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SmithAndSmithRegressionFunctional/SmithAndSmithTests/CreateAJob.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.Dynamics365.UIAutomation.Api;
+using System.Configuration;
+using OpenQA.Selenium;
+
+namespace Microsoft.Dynamics365.UIAutomation.Sample
+{
+    [TestClass]
+    public class CreateAJob
+    {
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void CreateANewJob()
+        {
+            using (var xrmBrowser = new Microsoft.Dynamics365.UIAutomation.Api.Browser(BrowserType.Chrome))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                xrmBrowser.ThinkTime(500);
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Jobs");
+
+                xrmBrowser.ThinkTime(3000);
+                xrmBrowser.CommandBar.ClickCommand("New");
+                xrmBrowser.ThinkTime(2000);
+
+                // test data source
+                //var jobData = ExcelDataAccess.GetTestData("TestCreateNewJob");
+                xrmBrowser.Entity.SetValue("dsl_suburbid", "0110 - Avenues");//jobData.Suburb);
+                xrmBrowser.Driver.FindElement(By.Id("dsl_suburbid")).SendKeys(Keys.Tab);
+                xrmBrowser.Entity.SetValue("dsl_servicelocationid_lookupValue", "Whangarei");//jobData.ServiceLocation);
+                xrmBrowser.Driver.FindElement(By.Id("dsl_servicelocationid_lookupValue")).SendKeys(Keys.Tab);
+                xrmBrowser.Entity.SetValue("dsl_accountid", "5611");//jobData.Account);
+                xrmBrowser.Driver.FindElement(By.Id("dsl_accountid")).SendKeys(Keys.Tab);
+                xrmBrowser.Entity.SetValue("dsl_registrationnumber", "HPZ495");// jobData.Rego);
+                xrmBrowser.Driver.FindElement(By.Id("dsl_registrationnumber")).SendKeys(Keys.Tab);
+                xrmBrowser.ThinkTime(2000);
+
+                // check if another iframe exisits
+                int size = xrmBrowser.Driver.FindElements(By.TagName("iframe")).Count;
+                if (size > 1)
+                {
+                    // if another frame exisits, then need to close the dialog
+                    xrmBrowser.Driver.SwitchTo().DefaultContent();
+                    xrmBrowser.ThinkTime(2000);
+                    if (xrmBrowser.Driver.IsVisible(By.Id("alertJs-divWarning")))
+                    {
+                        xrmBrowser.Driver.ClickWhenAvailable(By.Id("alertJs-close"));
+                        // xrmBrowser.Driver.FindElement(By.XPath(Elements.Xpath[Reference.Login.StaySignedIn])).Submit();
+                    }
+                }
+                xrmBrowser.ThinkTime(3000);
+                xrmBrowser.CommandBar.ClickCommand("Save & Close");
+                xrmBrowser.ThinkTime(2000);
+
+            }
+        }
+    }
+}
